dd387a3161831db2f7cd55cd8b847aa035476db3
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:48 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,8 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.blueimp.gallery.BlueImpGalleryPageConfigurator;
+import com.jwebmp.plugins.blueimp.gallery.implementations.BlueImpGalleryExclusionsModule;
 
 module com.jwebmp.plugins.blueimp.gallery {
 	exports com.jwebmp.plugins.blueimp.gallery;
@@ -8,11 +11,15 @@ module com.jwebmp.plugins.blueimp.gallery {
 
 	provides IPageConfigurator with BlueImpGalleryPageConfigurator;
 
+	provides IGuiceScanModuleExclusions with BlueImpGalleryExclusionsModule;
+	provides IGuiceScanJarExclusions with BlueImpGalleryExclusionsModule;
+
 	requires com.jwebmp.core;
 	requires java.validation;
 	requires com.fasterxml.jackson.annotation;
 	requires com.jwebmp.logmaster;
 	requires java.logging;
+	requires com.jwebmp.guicedinjection;
 
 	opens com.jwebmp.plugins.blueimp.gallery.options to com.jwebmp.guicedinjection, com.fasterxml.jackson.databind, com.google.guice, com.jwebmp.core;
 	opens com.jwebmp.plugins.blueimp.gallery.features to com.jwebmp.guicedinjection, com.fasterxml.jackson.databind, com.google.guice, com.jwebmp.core;
