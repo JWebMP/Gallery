99faccbd0f49a48e26897eab8ea427d836c98104
GedMarc <marc.magon@gmail.com>
2024-06-02 18:29:34 +0200

    Updates to GuicedEE V2

--- src/main/java/com/jwebmp/plugins/blueimp/gallery/BlueImpGalleryClasses.java
+++ src/main/java/com/jwebmp/plugins/blueimp/gallery/BlueImpGalleryClasses.java
@@ -5,35 +5,34 @@ import com.jwebmp.core.base.interfaces.ICssClassName;
 
 import static com.guicedee.services.jsonrepresentation.json.StaticStrings.CHAR_DASH;
 import static com.guicedee.services.jsonrepresentation.json.StaticStrings.CHAR_UNDERSCORE;
-import static com.jwebmp.core.utilities.StaticStrings.*;
 
 public enum BlueImpGalleryClasses
-		implements ICssClassName, AttributeDefinitions
+        implements ICssClassName, AttributeDefinitions
 {
-	BlueImp_Gallery,
-	BlueImp_Gallery_Controls,
-	BlueImp_Gallery_Carousel,
+    BlueImp_Gallery,
+    BlueImp_Gallery_Controls,
+    BlueImp_Gallery_Carousel,
 
-	Data_Gallery,
+    Data_Gallery,
 
-	Slides,
-	Title,
-	Prev,
-	Next,
-	Close,
-	Play_Plause,
-	Indicator;
+    Slides,
+    Title,
+    Prev,
+    Next,
+    Close,
+    Play_Plause,
+    Indicator;
 
-	@Override
-	public String toString()
-	{
-		return name().toLowerCase()
-		             .replace(CHAR_UNDERSCORE, CHAR_DASH);
-	}
+    @Override
+    public String toString()
+    {
+        return name().toLowerCase()
+                     .replace(CHAR_UNDERSCORE, CHAR_DASH);
+    }
 
-	@Override
-	public boolean isKeyword()
-	{
-		return false;
-	}
+    @Override
+    public boolean isKeyword()
+    {
+        return false;
+    }
 }
